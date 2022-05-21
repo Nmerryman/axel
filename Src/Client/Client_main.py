250ffafbc3567c7fc79bfb1bf08fe5cc53f369d8
Nmerryman <Nmerryman9001@gmail.com>
2022-05-21 18:50:54 -0500

    Added alternative anchor location for development purposes.

--- Src/Client/Client_main.py
+++ Src/Client/Client_main.py
@@ -1,4 +1,13 @@
 import Services.loader as loader
+from Services.data_structures import FileToken
 import os
 
-print(loader.get_anchor())
+anchor = loader.get_anchor()
+# If in a testing environment, use alternative location
+if anchor == "DEV":
+    from pathlib import Path
+    loader.DATA_PATH = Path("dev_data")
+    loader.get_anchor()
+
+
+
