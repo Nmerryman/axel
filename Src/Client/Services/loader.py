29791b63e8627e5fdc0e8847dca2cd575d151ac6
Nmerryman <Nmerryman9001@gmail.com>
2022-05-21 03:14:56 -0500

    Cleaned up data structure, which can be built through loader now

--- Src/Client/Services/loader.py
+++ Src/Client/Services/loader.py
@@ -6,14 +6,32 @@ from pathlib import Path
 This indexes and loads user info
 """
 
-DATA_PATH = Path("Data")
+DATA_PATH = Path("data")  # Where all user data is stored
+anchor = "anchor.txt"  # Name of anchor file
 
 
 def get_anchor():
     """
     This is more to test that loader can find the right directory
+    It will check what mode to run in based on text in anchor.txt and generate any missing files/directories.
     :return: Anchor text
     """
-    with open(DATA_PATH / "anchor.txt", "r") as f:
-        text = f.read()
+
+    DATA_PATH.mkdir(exist_ok=True)  # Data folder exists
+
+    # Anchor file exists and get text
+    if not (DATA_PATH / anchor).exists():
+        with open(DATA_PATH / anchor, 'w') as f:
+            f.write("GENERATED")
+            text = "GENERATED"
+    else:
+        with open(DATA_PATH / anchor, "r") as f:
+            text = f.read()
+
+    # make sure all needed files are here
+    (DATA_PATH / "storage").mkdir(exist_ok=True)
+    (DATA_PATH / "user").mkdir(exist_ok=True)
+    (DATA_PATH / "user" / "tokens").touch(exist_ok=True)
+    (DATA_PATH / "user" / "gates").touch(exist_ok=True)
+
     return text
