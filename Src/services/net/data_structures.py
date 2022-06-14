4ed6f27eb64a258ff30818476e9420711fa0a40c
Nmerryman <Nmerryman9001@gmail.com>
2022-06-14 01:05:12 -0500

    Added a net submodule for network connections. This adds a server model I used before

---
+++ Src/services/net/data_structures.py
@@ -0,0 +1,85 @@
+import json
+import enum
+
+
+DEBUG = False
+
+
+def log(*text):
+    if DEBUG:
+        print("D:", *text)
+
+
+# We are assuming entire packet fits in recv size. May need to add proper data buffering/loading otherwise
+# It is not super optimised for readability
+class Packet:
+
+    def __init__(self, data_type=None, value=None, data=None, extra=None):
+        if not data_type:
+            data_type = ''
+        if not value:
+            value = ''
+        if not data:
+            data = ''
+        if not extra:
+            extra = ''
+        self.storage = {"TYPE": data_type, "VALUE": value, "DATA": data, "EXTRA": extra}
+        self.type = data_type
+        self.value = value
+        self.data = data
+        self.extra = extra
+
+    def __getitem__(self, name: str):
+        return self.storage[name.upper()]
+
+    def load_storage(self):
+        self.type = self.storage["TYPE"]
+        self.value = self.storage["VALUE"]
+        self.data = self.storage["DATA"]
+        self.extra = self.storage["EXTRA"]
+
+    def set_value(self, value):
+        self.value = value
+        self.storage["VALUE"] = value
+
+    def set_type(self, data_type):
+        self.type = data_type
+        self.storage["TYPE"] = data_type
+
+    def set_data(self, data):
+        self.data = data
+        self.storage["DATA"] = data
+
+    def set_extra(self, extra):
+        self.extra = extra
+        self.storage["EXTRA"] = extra
+
+    def generate(self):
+        return json.dumps(self.storage).encode()
+
+    def parse(self, data):
+        check = str(data, "utf-8")
+        if check == "":
+            self.storage = Packet().storage  # use empty default
+        else:
+            self.storage = json.loads(check)
+
+        self.load_storage()
+        log(str(data, "utf-8"))
+        return self
+
+
+class MessageType(enum.IntEnum):
+    command = enum.auto()
+    text = enum.auto()
+    empty = enum.auto()
+
+
+class Commands(enum.IntEnum):
+    greet = enum.auto()
+    close = enum.auto()
+    exit = enum.auto()
+    is_alive = enum.auto()
+    request = enum.auto()
+    broadcast = enum.auto()
+    unblock = enum.auto()
---
+++ Src/services/net/server.py
@@ -0,0 +1,113 @@
+import threading
+import time
+import socket
+from Src.services.net.data_structures import *
+
+
+class Server:
+    """
+    Model:
+    each function gets handed proxy access to the main thread
+    todo Consider any Packets moving around internally as always being command to free up another field
+    todo Provide better ways to share data between clients
+    """
+    def __init__(self, root_port: int, incoming_handler):
+        log("starting")
+        self.alive = True
+        self.root_port = root_port
+        self.used_ports = [root_port]
+        self.clients = {}  # {id: (conn, ip)}
+        self.running_threads = []
+        self.handle = incoming_handler
+        self.enforce_broadcast_exclude = True
+
+        self.running_threads.append(threading.Thread(target=self.root_thread))
+        self.running_threads[0].start()
+        log(f"started root thread and listening on 127.0.0.1:{self.root_port}")
+
+    def mainloop(self):
+        # The point of this to try to handle closing all threads when there is no custom server stuff needed.
+
+        # This belongs to the server and will not get blocked or be involved with the sockets
+        log("in mainloop")
+
+        # Self kill test after enough time
+        # while self.alive:
+        #     if len(self.running_threads) > 2:
+        #         self.shutdown()
+
+        # Not sure if the mainloop needs to stay alive
+        while self.alive:
+            try:
+                time.sleep(1)
+            except:  # We want to catch ctrl-c
+                self.alive = False
+
+        # Kill the mainloop once all other threads are dead
+        while True:
+            all_dead = True
+            for num_a, a in enumerate(self.running_threads):
+                if a.is_alive():
+                    log(f"Thread {num_a} is still alive")
+                    all_dead = False
+
+            if all_dead:
+                break
+
+            time.sleep(1)
+
+    def root_thread(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("127.0.0.1", self.root_port))
+        s.listen()
+        while self.alive:
+            conn, ip = s.accept()
+            log(f"Connection from {ip}")
+            if self.alive:
+                t = threading.Thread(target=self.thread_base, args=(self.handle, conn, ip, self.proxy, len(self.running_threads)))
+                t.start()
+                self.running_threads.append(t)
+
+    def thread_base(self, func, conn, ip, proxy, cid):
+        # Might not even need this base
+        self.clients[cid] = (conn, ip)
+        func(conn, ip, cid, proxy)
+        self.clients.pop(cid)
+        log(f"Ending {ip}")
+
+    def proxy(self, command):
+        # Allow a thread to use commands from the main thread
+        data = Packet().parse(command)
+        if data.type == MessageType.command:
+            if data.value == Commands.exit:
+                self.alive = False
+                self.shutdown()
+            elif data.value == Commands.is_alive:
+                return self.alive
+            elif data.value == Commands.broadcast and data.extra:
+                self.broadcast(data.generate(), exclude=int(data.extra))
+
+    def pseudo_connection(self, port):
+        ks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ks.connect(("127.0.0.1", port))
+        ks.close()
+
+    def shutdown(self):
+        # Kill what we can and then do some extra checking
+        self.alive = False
+        self.pseudo_connection(self.root_port)  # unblock listener thread
+
+        # everything should clean itself up now. Not sure if I want to forcibly kill active threads
+
+    def broadcast(self, data, exclude=-1):
+        """
+        Send Packet to all connections (except excluded)
+        """
+        for cid, (conn, ip) in self.clients.items():
+            if cid != exclude or not self.enforce_broadcast_exclude:
+                conn.sendall(data)
+
+    def unblock(self, cid):
+        if cid in self.clients.keys():
+            conn = self.clients[cid][0]
+            conn.sendall(Packet().generate())
