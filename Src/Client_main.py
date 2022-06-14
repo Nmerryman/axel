dd324330f7af1ea9d46c024f58aab19636b58c18
Nmerryman <Nmerryman9001@gmail.com>
2022-06-14 00:36:37 -0500

    restructured the file to work better with command line arguments

--- Src/Client_main.py
+++ Src/Client_main.py
@@ -2,6 +2,8 @@ import services.loader as loader
 from services.data_structures import FileToken
 from services.gen_dev_data import *
 import os
+import argparse
+
 
 anchor = loader.get_anchor()
 # If in a testing environment, use alternative location
@@ -10,9 +12,24 @@ if anchor == "DEV":
     loader.DATA_PATH = Path("dev_data")
     loader.get_anchor()
 
-loader.store_file_tokens([file_token_default()])
-l = loader.load_file_tokens()
-t = file_token_default()
-t.file_name = "e"
-print(t == l[0])
+
+def main():
+
+    loader.store_file_tokens([file_token_default()])
+    l = loader.load_file_tokens()
+    t = file_token_default()
+    t.file_name = "e"
+    print(t == l[0])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gate", action="store_true", help="Use client as an entry point")
+    parser.add_argument("--root", action="store_true", help="Act as the first entry point with only knowledge of self")
+
+    args = parser.parse_args()
+    if args.gate:
+        print("Acting as gate")
+    else:
+        print("Not a gate")
 
