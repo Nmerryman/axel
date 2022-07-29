216c0be0760e4eab2041ad0324e1aac32a9c600c
Nmerryman <nmerryman9001@gmail.com>
2022-07-29 17:35:12 -0500

    Started to impliment wrapped socket connections

--- axel/services/net/data_structures.py
+++ axel/services/net/data_structures.py
@@ -1,6 +1,8 @@
 import json
 import enum
-
+import socket
+from string import ascii_letters
+import select
 
 DEBUG = True
 
@@ -70,6 +72,63 @@ class Packet:
         self.load_storage()
         log(str(data, "utf-8"))
         return self
+
+
+class WrappedConnection:
+
+    def __init__(self, connection: socket.socket):
+        self.conn = connection
+        self.conn.setblocking(False)
+        self.partial = b''
+        self.finished = []
+        self.recv_size = 1000
+        self.mode = "packet"
+        # Packet mode takes advantage of json formatting to detect start and ends of packets and then rebuilds the original messages
+        # Stream mode is more dynamic
+        #     First byte dictates how many bytes are needed to represent the message
+        #     Next n bytes is the exact length of the message
+        #     Finally the message gets sent
+        #       In theory this system can send up to 18 exabytes of data per message
+        self.alive = True
+
+    def load_awaited(self):
+        data = ""
+        first = True
+        readable = []
+        while (first or readable) and self.alive:
+            print(self.partial)
+            readable, _, errors = select.select([self.conn], [], [self.conn], 0)
+            if readable:
+                self.partial += self.conn.recv(self.recv_size)
+                readable = []
+            first = False
+
+    def parse_partial(self):
+        if self.mode == "packet":
+            depth = 0
+            starts = []
+            ends = []
+            for num_a, a in enumerate(self.partial):
+                if a == ord(b'{'):
+                    if depth == 0:
+                        starts.append(num_a)
+                    depth += 1
+                if a == ord(b'}'):
+                    depth -= 1
+                    if depth == 0:
+                        ends.append(num_a)
+            if len(starts) == len(ends) and len(ends) > 0:
+                for s, e in zip(starts, ends):
+                    self.finished.append(Packet().parse(self.partial[s:e+1]))
+                self.partial = self.partial[ends[-1]+1:]
+
+    def load_next(self):
+        found = False
+        data = ""
+        while not found or data:
+            data = self.conn.recv(self.recv_size)
+            if data:
+                found = True
 
 
 class MessageType(enum.IntEnum):
