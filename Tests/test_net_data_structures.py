79608dbe2d998dfa65ad01b0b089e9018477bdbe
Nmerryman <nmerryman9001@gmail.com>
2022-07-29 21:25:28 -0500

    Added some tests for my wrapped sockets

--- Tests/test_net_data_structures.py
+++ Tests/test_net_data_structures.py
@@ -1,4 +1,8 @@
 import axel.services.net.data_structures as ds
+import axel.services.net.server as serv
+import socket
+from functools import partial
+from time import sleep
 
 
 def test_packet_init():
@@ -26,3 +30,73 @@ def test_packet_gen_and_parse():
     assert len(gen) > 10
     new = ds.Packet().parse(gen)
     assert new == p
+
+
+def client_handle(message: bytes, conn: socket.socket, *args):
+    conn.sendall(message)
+    conn.close()
+
+
+def test_wrapped_partial():
+    """
+    We can parse a single set packet
+    """
+    wrap = ds.WrappedConnection(None)  # None will not work with real sockets
+    assert not wrap.partial  # Starts with nothing
+    assert not wrap.finished
+    wrap.partial = ds.Packet(1).generate()
+    assert wrap.partial  # Something is queued up
+    wrap.parse_partial()
+    assert not wrap.partial  # Everything gets parsed and mooved
+    assert wrap.finished
+    assert wrap.finished[0] == ds.Packet(1)
+
+
+def test_two_wrapped_partial():
+    """
+    We can send and parse multiple packets
+    """
+    wrap = ds.WrappedConnection(None)
+    wrap.partial = ds.Packet(1).generate() + ds.Packet(2).generate()
+    assert wrap.partial
+    wrap.parse_partial()
+    assert len(wrap.finished) == 2
+    assert wrap.finished[1].type == 2
+
+
+def test_partial_wrapped_partial():
+    """
+    Show we can parse all finished packets in the partial queue and leave incomplete ones
+    """
+    wrap = ds.WrappedConnection(None)
+    p = ds.Packet(1)
+    payload = p.generate() * 2
+    cut = payload[:-5]
+    wrap.partial = cut
+    wrap.parse_partial()
+    assert len(wrap.finished) == 1
+    assert wrap.finished[0] == p
+    assert wrap.partial == p.generate()[:-5]
+
+
+def test_parse_sent_packet():
+    port = serv.get_first_port_from(13131)
+    p = ds.Packet(1, 2)
+    s = serv.Server(port, partial(client_handle, p.generate()))
+
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect(("localhost", port))  # connecting is a blocking only operation so it must be done first
+    wrap = ds.WrappedConnection(conn)
+
+    sleep(.01)  # Socket has some delay needed
+    assert not wrap.partial
+    wrap.load_awaited()
+    assert wrap.partial
+    wrap.parse_partial()
+    assert not wrap.partial
+    assert wrap.finished[-1] == p
+
+    s.shutdown()
+
+
+
